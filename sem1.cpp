e7f8abfe8b543613f7d60f98f5925d6014311da0
Maciej Flaga <maciejjjj4@gmail.com>
2026-03-03 09:53:51 +0100

    initial commit

---
+++ sem1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <complex>
+
+using namespace std;
+
+int main(){
+    // parametry
+    const int N=75;
+    const int Nt=20;
+    const double xmin = -1.0;
+    const double xmax = 1.0;
+    const double dx = (xmax-xmin)/N;
+    // alokacja
+    double *x = new double[N+1];
+    complex<double> **psi = new complex<double>*[N+1];
+    for (int i=0;i<=N;i++){
+        psi[i] = new double[Nt+1];
+    }
+    // czystki
+    delete [] x;
+    delete [] psi;
+    return 0;
+}
