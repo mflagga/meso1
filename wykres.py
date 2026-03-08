5fa46d7be5ea477a218bf575e8ce3d8eb1fabae4
Maciej Flaga <maciejjjj4@gmail.com>
2026-03-08 21:06:19 +0100

    fixed y values

--- wykres.py
+++ wykres.py
@@ -13,12 +13,14 @@ frame_dir.mkdir(parents=True, exist_ok=True)
 plt.figure(figsize=(8,6))
 
 n=0
+psimax = max(dane[:,2])
 
 for t in tu:
     mask = dane[:,0]==t
     plt.plot(dane[mask,1],dane[mask,2])
     #plt.tight_layout()
     plt.title(f't={t:.3f}')
+    plt.ylim(0,psimax)
     filename = frame_dir / f"frame_{n:04d}.png"
     n+=1
     plt.savefig(filename)
